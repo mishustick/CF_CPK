f77fe0ac0a06cec794345bdd7e2122efd5328f1c
mishustick <mikh.petrov@bk.ru>
2023-07-24 10:47:11 +0300

    Update send_rec.py
    Добавил запись отправки в файл

--- send_rec.py
+++ send_rec.py
@@ -6,58 +6,76 @@ import time
 from cls_pr import Message
 
 
-def sending(UDP_IP, UDP_PORT):
-    pass
-
-
 # Запись в файл
 def saver(que):
     print('Saver запущен!')
     while True:
         items = que.get()
-        fi = open('log_rec.txt', 'a')
+        f_rec = open('log_rec.txt', 'a')
+        f_sen = open('log_sen.txt', 'a')
         for el in items:
-            fi.write(str(el.time)+','+str(el)+'\n')
-            time.sleep(0.05)
-        fi.close()
+            if el.header == 57:
+                f_rec.write(str(el.time)+';'+str(el)+str(el.header)+'\n')
+            elif el.header == 66:
+                f_sen.write(str(el.time)+';'+str(el)+str(el.header)+'\n')
+        f_rec.close()
+        f_sen.close()
 
 
 def receiving(UDP_IP, UDP_PORT):
     sock = socket.socket(socket.AF_INET,  # Internet
                          socket.SOCK_DGRAM)  # UDP
     sock.bind((UDP_IP, UDP_PORT))
-    n = 0  # Счетчик сообщений
-    global data_rec, curr_time
+    data_rec = []
     print('Получение запущено!')
     while True:
         data, addr = sock.recvfrom(1024)  # buffer size is 1024 bytes
         m = Message.bts2dbl(data)
         print(m.time, m.na())
         data_rec.append(m)
-        n += 1  # Увеличиваем количество полученных сообщений
-        if n == 100 or time.perf_counter() - curr_time > 10:
+        queue_sen.put(m)  # Передаем сообщение на отправку
+        if len(data_rec) == 100:
             n = 0
-            queue.put(data_rec)  # Добавляем текущий кэш сообщений в очередь на запись
+            queue_rec.put(data_rec)  # Добавляем текущий кэш сообщений в очередь на запись
             data_rec = []  # Сбрасываем текущий кэш сообщений
-            curr_time = time.perf_counter()  # Обновляем время
+
+
+def sending(UDP_IP, UDP_PORT, que_sen):
+    print('Отправление запущено!')
+    print("UDP target IP: %s" % UDP_IP)
+    sock = socket.socket(socket.AF_INET,  # Internet
+                         socket.SOCK_DGRAM)  # UDP
+    data_sen = []
+    while True:
+        item = que_sen.get()
+        item.dbl2bts()
+        s_m = Message.bts2dbl(item.bts)
+        data_sen.append(s_m)
+        #sock.sendto(item.bts, (UDP_IP, UDP_PORT))  # Отправляем в виде 66 + 15 * 0 + 7 п
+        if len(data_sen) == 100:
+            queue_rec.put(data_sen)
+            data_sen = []
 
 
 curr_time = time.perf_counter()
 
 res_ip = "192.168.1.64"
 sen_ip = "192.168.1.64"
-data_rec = []
-port = 5005
-queue = Queue()
+port = 5004
+queue_rec = Queue()
+queue_sen = Queue()
 
-f = open('log_rec.txt', 'w')
-f.write('time,nx,ny,nz,phix,phiy,phiz,gref\n')
-f.close()
+f_r = open('log_rec.txt', 'w')
+f_r.write('time;nx;ny;nz;phix;phiy;phiz;gref;header\n')
+f_r.close()
 
-t_sending = threading.Thread(target=sending, args=(sen_ip, port))
+f_s = open('log_sen.txt', 'w')
+f_s.write('time;nx;ny;nz;phix;phiy;phiz;gref;header\n')
+f_s.close()
+
+t_sending = threading.Thread(target=sending, args=(sen_ip, port, queue_sen))
 t_reseveing = threading.Thread(target=receiving, args=(res_ip, port))
-t_sav = threading.Thread(target=saver, args=(queue,))
-
+t_sav = threading.Thread(target=saver, args=(queue_rec,))
 
 t_sav.start()
 t_reseveing.start()
